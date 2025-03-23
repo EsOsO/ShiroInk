f442ae694054f1d49822c529d735787fac314471
Massimo Bonvicini <EsOsO@users.noreply.github.com>
2025-03-23 18:59:22 +0100

    remove image quantize

--- src/manga_image_pipeline/sharpen.py
+++ src/manga_image_pipeline/sharpen.py
@@ -9,11 +9,4 @@ def sharpen_image(image: Image) -> Image:
 
 
 def apply_grey_palette(image: Image, palette) -> Image:
-    colors = len(palette) // 3
-    if colors < 256:
-        palette = palette + palette[:3] * (256 - colors)
-
-    pal_img = Image.new("P", (1, 1))
-    pal_img.putpalette(palette)
-
-    return image.quantize(palette=pal_img)
+    return image.quantize(colors=16)
